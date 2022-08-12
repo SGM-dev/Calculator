a6cdba44ffed911c0132d51e0c9ee4e3c918446e
Sam George Mathew <sam.mathew19113@gmail.com>
2022-08-12 18:37:04 +0530

    define AC

--- script.js
+++ script.js
@@ -2,12 +2,32 @@ class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
+    this.clear()
   }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+
+  delete() {}
+
+  appendNumber(number) {}
+  chooseOperation(operation) {}
+
+  compute() {}
+
+  updateDisplay() {}
 }
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
 const deleteButton = document.querySelector("[data-delete]");
 const allClearButton = document.querySelector("[data-all-clear]");
-const previousOperandButton = document.querySelector("[data-previous-operand]");
-const currentOperandButton = document.querySelector("[data-current-operand]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
